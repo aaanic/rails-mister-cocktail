017cb3143f30b0aa83c9171235203eac26cb8713
Anic Fraefel <anicfraefel@gmail.com>
2019-08-16 08:26:05 +0200

    missing the redirects, other than that solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'cocktails', to: 'cocktails#index', as: 'cocktails'
+  post 'cocktails', to: 'cocktails#create'
+  get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: 'new_dose'
+  post 'cocktails/:cocktail_id/doses', to: 'doses#create', as: 'cocktail_doses'
+  delete 'cocktails/:cocktail_id/doses/:id', to: 'doses#destroy', as: 'delete_dose'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "basil")
+Ingredient.create(name: "angostura")
+
+Cocktail.create(name: "Basil Smash")
+Cocktail.create(name: "Piña Colada")
+Cocktail.create(name: "Having fun at the beach")
+Cocktail.create(name: "Coconut Rush")
+Cocktail.create(name: "Ipanema")
+Cocktail.create(name: "Lemon Lime Bitters")
